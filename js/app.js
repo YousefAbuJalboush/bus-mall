c79e7bd72a706b5a7781add661ad7e769b717003
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-04-18 16:51:18 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -37,26 +37,26 @@ function BusMall(name, source) {
 
 BusMall.allObject = [];
 
-new BusMall('bag', '../img/bag.jpg');
-new BusMall('banana', '../img/banana.jpg');
-new BusMall('bathroom', '../img/bathroom.jpg');
-new BusMall('boots', '../img/boots.jpg');
-new BusMall('breakfast', '../img/breakfast.jpg');
-new BusMall('bubblegum', '../img/bubblegum.jpg');
-new BusMall('chair', '../img/chair.jpg');
-new BusMall('cthulhu', '../img/cthulhu.jpg');
-new BusMall('dog-duck', '../img/dog-duck.jpg');
-new BusMall('dragon', '../img/dragon.jpg');
-new BusMall('pen', '../img/pen.jpg');
-new BusMall('pet-sweep', '../img/pet-sweep.jpg');
-new BusMall('scissors', '../img/scissors.jpg');
-new BusMall('shark', '../img/shark.jpg');
-new BusMall('sweep', '../img/sweep.png');
-new BusMall('tauntaun', '../img/tauntaun.jpg');
-new BusMall('unicorn', '../img/unicorn.jpg');
-new BusMall('usb', '../img/usb.gif');
-new BusMall('water-can', '../img/water-can.jpg');
-new BusMall('wine-glass', '../img/wine-glass.jpg');
+new BusMall('bag', 'img/bag.jpg');
+new BusMall('banana', 'img/banana.jpg');
+new BusMall('bathroom', 'img/bathroom.jpg');
+new BusMall('boots', 'img/boots.jpg');
+new BusMall('breakfast', 'img/breakfast.jpg');
+new BusMall('bubblegum', 'img/bubblegum.jpg');
+new BusMall('chair', 'img/chair.jpg');
+new BusMall('cthulhu', 'img/cthulhu.jpg');
+new BusMall('dog-duck', 'img/dog-duck.jpg');
+new BusMall('dragon', 'img/dragon.jpg');
+new BusMall('pen', 'img/pen.jpg');
+new BusMall('pet-sweep', 'img/pet-sweep.jpg');
+new BusMall('scissors', 'img/scissors.jpg');
+new BusMall('shark', 'img/shark.jpg');
+new BusMall('sweep', 'img/sweep.png');
+new BusMall('tauntaun', 'img/tauntaun.jpg');
+new BusMall('unicorn', 'img/unicorn.jpg');
+new BusMall('usb', 'img/usb.gif');
+new BusMall('water-can', 'img/water-can.jpg');
+new BusMall('wine-glass', 'img/wine-glass.jpg');
 
 // console.log(BusMall.allObject);
 
