1cfe221aacf46f15a9e792d11989a8d23874a74c
YousefAbu-Jalboush <yousef.y.jalboush@gmail.com>
2021-04-19 14:07:33 +0300

    finsh chartjs

--- js/app.js
+++ js/app.js
@@ -22,10 +22,17 @@ buttonResults.addEventListener('click', resultsImg);
 let counts = 0;
 let maxAttempts = 25;
 
-let leftIndex;
-let centerIndex;
-let rightIndex;
+// let leftIndex;
+// let centerIndex;
+// let rightIndex;
 
+let firstShowIndex = [];
+
+let arrayOfIndex = [];
+
+let busMallName = [];
+let busMallVotes = [];
+let busMallShow = [];
 
 function BusMall(name, source) {
   this.name = name;
@@ -33,6 +40,7 @@ function BusMall(name, source) {
   this.votes = 0;
   this.show = 0;
   BusMall.allObject.push(this);
+  busMallName.push(this.name);
 }
 
 BusMall.allObject = [];
@@ -64,30 +72,45 @@ renderImages();
 
 function renderImages() {
 
-  leftIndex = getRandomNum();
-  centerIndex = getRandomNum();
-  rightIndex = getRandomNum();
+  firstShowIndex[0] = getRandomNum();
+  firstShowIndex[1] = getRandomNum();
+  firstShowIndex[2] = getRandomNum();
 
+  // leftIndex = getRandomNum();
+  // centerIndex = getRandomNum();
+  // rightIndex = getRandomNum();
 
-  while (leftIndex === centerIndex || leftIndex === rightIndex || centerIndex === rightIndex) {
-    if (leftIndex === centerIndex) {
-      centerIndex = getRandomNum();
-    } else if (leftIndex === rightIndex) {
-      rightIndex = getRandomNum();
-    } else if (centerIndex === rightIndex) {
-      rightIndex = getRandomNum();
+  // firstShowIndex = [];
+
+  while ( firstShowIndex.some( check => arrayOfIndex.includes(check) ) || firstShowIndex[0] === firstShowIndex[1] || firstShowIndex[0] === firstShowIndex[2] || firstShowIndex[1] === firstShowIndex[2]) {
+    if (firstShowIndex[0] === firstShowIndex[1]) {
+      firstShowIndex[1] = getRandomNum();
+    } else if (firstShowIndex[0] === firstShowIndex[2]) {
+      firstShowIndex[2] = getRandomNum();
+    } else if (firstShowIndex[1] === firstShowIndex[2]) {
+      firstShowIndex[2] = getRandomNum();
+    } else if ( arrayOfIndex.includes(firstShowIndex[0]) ) {
+      firstShowIndex[0] = getRandomNum();
+    }
+    else if ( arrayOfIndex.includes(firstShowIndex[1]) ) {
+      firstShowIndex[1] = getRandomNum();
+    }
+    else if ( arrayOfIndex.includes(firstShowIndex[2]) ) {
+      firstShowIndex[2] = getRandomNum();
     }
   }
 
-  BusMall.allObject[leftIndex].show++;
-  BusMall.allObject[centerIndex].show++;
-  BusMall.allObject[rightIndex].show++;
+  arrayOfIndex = [firstShowIndex[0] , firstShowIndex[1] , firstShowIndex[2]];
 
-  leftImg.src = BusMall.allObject[leftIndex].source;
+  BusMall.allObject[firstShowIndex[0]].show++;
+  BusMall.allObject[firstShowIndex[1]].show++;
+  BusMall.allObject[firstShowIndex[2]].show++;
 
-  centerImg.src = BusMall.allObject[centerIndex].source;
+  leftImg.src = BusMall.allObject[firstShowIndex[0]].source;
 
-  rightImg.src = BusMall.allObject[rightIndex].source;
+  centerImg.src = BusMall.allObject[firstShowIndex[1]].source;
+
+  rightImg.src = BusMall.allObject[firstShowIndex[2]].source;
 
 }
 
@@ -98,11 +121,11 @@ function onClicking(event) {
   if (maxAttempts >= counts) {
 
     if (event.target.id === 'leftImg') {
-      BusMall.allObject[leftIndex].votes++;
+      BusMall.allObject[firstShowIndex[0]].votes++;
     } else if (event.target.id === 'centerImg') {
-      BusMall.allObject[centerIndex].votes++;
+      BusMall.allObject[firstShowIndex[1]].votes++;
     } else if (event.target.id === 'rightImg'){
-      BusMall.allObject[rightIndex].votes++;
+      BusMall.allObject[firstShowIndex[2]].votes++;
     }
 
     renderImages();
@@ -113,18 +136,53 @@ function onClicking(event) {
     centerImg.removeEventListener('click', onClicking);
     rightImg.removeEventListener('click', onClicking);
 
+
+    buttonResults.style.display = 'block';
   }
 }
 
 
-function resultsImg( ) {
+function resultsImg() {
   let ulResults = document.getElementById('ulResults');
 
   ulResults.innerHTML = '';
 
   for(let i = 0 ; i < BusMall.allObject.length;i++){
+
+    busMallVotes.push( BusMall.allObject[i].votes );
+    busMallShow.push( BusMall.allObject[i].show );
+    // console.log(busMallVotes);
+
     let li = document.createElement('li');
     ulResults.appendChild(li);
     li.textContent = `${BusMall.allObject[i].name} had ${BusMall.allObject[i].votes} votes, and was seen ${BusMall.allObject[i].show} times.`;
   }
+  addChart();
+}
+
+
+function addChart() {
+
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: busMallName, // ['goat away' ,  ... 'sassy goat']
+      datasets: [{
+        label: 'Number Of votes',
+        data: busMallVotes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+        ],
+        borderWidth: 1
+      },{
+        label:'# of Shown',
+        data: busMallShow,
+        backgroundColor:[
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderWidth: 1
+      }]
+    }
+  });
 }
